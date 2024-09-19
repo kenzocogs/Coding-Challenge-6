9035462a2ee7eb3513cc302b8ad4e5af9823ee29
Kenzo Cogswell <kcogswell@usf.edu>
2024-09-18 22:12:32 -0400

    Initialize employees with shift schedules

---
+++ ShiftSystem.js
@@ -0,0 +1,16 @@
+// Task 1: Initialize employees with shift schedules
+
+let employees = [
+{ name: 'Kenzo', shifts: 
+    [{ day: 'Tuesday', hours: 6}, { day: 'Friday', hours: 4}] },
+    
+{ name: 'Billybob', shifts: 
+    [{ day: 'Wednesday', hours: 10}, { day: 'Thursday', hours: 3}] },
+
+{ name: 'Claudia', shifts: 
+    [{ day: 'Monday', hours: 8} ]},
+
+{ name: 'Britney', shifts: 
+    [{ day: 'Friday', hours: 9}] },
+
+]
